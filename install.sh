1d8f0c2898747d38dcbc85de67c1c3dfbd6bf911
TerrySolar <531012201@qq.com>
2018-11-28 09:40:47 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -11,17 +11,29 @@ done
 echo 'tic terminale 256 color done!'
 
 
-if [ -e "$HOME/.tmux.conf"]; then
+if [ -e "$HOME/.tmux.conf" ]; then
     echo "found .tmux.conf in your home directory, create a backup at $HOME/.tmux.conf.bak\n"
     cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null
 fi
 
-if [ -e "$HOME/.vimrc"]; then
+if [ -e "$HOME/.vimrc" ]; then
     echo "found .vimrc in your home directory, create a backup at $HOME/.vimrc.bak\n"
     cp -f "$HOME/.vimrc" "$HOME/.vimrc.bak" 2>/dev/null
 fi
 
-if [ -d "$HOME/.vim"]; then
+if [ -d "$HOME/.vim" ]; then
     echo "found .vim directory in your home directory, create a backup at $HOME/.vim.bak\n"
     cp -rf "$HOME/.vim" "$HOME/.vim.bak" 2>/dev/null
 fi
+
+
+if [ ! -d "./.vim" ]; then
+    tar -zxf vim.tar.gz
+fi
+
+rm -rf "$HOME/.vim"
+cp -rf .vim "$HOME/.vim"
+cp -rf tmux.conf "$HOME/.tmux.conf"
+cp -rf vimrc "$HOME/.vimrc"
+
+echo "env setup done!!!"
